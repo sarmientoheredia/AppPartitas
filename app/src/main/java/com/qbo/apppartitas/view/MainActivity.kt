82177e54dfd5b3d9615913ba64c0c91004a25323
Sarmiento <sarmiheredia2@gmail.com>
2021-04-20 18:57:34 -0500

    inicio de la aplicacion para el consumo de apis

---
+++ app/src/main/java/com/qbo/apppartitas/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.qbo.apppartitas.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qbo.apppartitas.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
